e870b92a716718c9781b4b39c5af2deed56155bd
Gustavo Camargo <camargogustavoa@gmail.com>
2022-10-25 09:32:31 -0500

    First commit (Base project added

---
+++ javascript/collectData.js
@@ -0,0 +1,23 @@
+//Player creation
+let PlayerList =  [];
+
+
+class Player {
+    constructor(nick,figure) {
+        this.nick = nick;
+        this.figure = figure
+    }
+}
+
+function collectData() {
+    let nickName =  document.getElementById("name").value;
+    let figure = document.querySelector('#figure');
+
+    console.log(figure)
+    PlayerList.push(new Player(nickName,figure));
+
+    console.log(PlayerList[0].nick + PlayerList[0].figure);
+}
+//La funcion hace efecto al darle click al botón
+const button = document.getElementById("start");
+button.addEventListener('click',collectData);
